5a099122005948ee90ae474189f7b05183b423a7
Ilja <ilja.vitohins@gmail.com>
2021-11-24 13:39:38 +0200

    Solution using Java Stream

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,47 +1,36 @@
 package com.company;
-
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.util.*;
 import java.util.stream.Stream;
 
 public class Main {
-
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("C:\\Users\\karin\\Desktop\\Random Book.txt");
         Scanner scanner = new Scanner(file);
         System.out.println(file.getName());
         List<String> text = new ArrayList<>();
-
         while (scanner.hasNext()) {
             String x = scanner.next();
             text.add(x);
         }
+        List<String> filteredWords = new ArrayList<>();
         for (String words : text) {
             String[] sepWords = words.split("\\P{Alpha}+");
-//            System.out.println(Arrays.toString(sepWords));
-
+            filteredWords.add(Arrays.toString(sepWords));
         }
-        String[] uniqueWords = text.stream().distinct().
-                toArray(String[]::new);
-        Map<String, Integer> wordsMap = new HashMap<>();
-        int frequency = 0;
-//        //Load the words to Map with each uniqueword as Key and frequency as Value
+        HashMap<String, Integer> uniqueWordMap = new HashMap<>();
+        String[] uniqueWords = filteredWords.stream().distinct().toArray(String[]::new);
+        int frequency;
         for (String uniqueWord : uniqueWords) {
-            frequency = Collections.frequency(text, uniqueWord);
-            System.out.println(uniqueWord + " occured " + frequency + " times");
-            wordsMap.put(uniqueWord, frequency);
+            frequency = Collections.frequency(filteredWords, uniqueWord);
+//            System.out.println(uniqueWord + " - " + frequency);
+            uniqueWordMap.put(uniqueWord, frequency);
         }
-//
-//        //Now, Sort the words with the reverse order of frequency(value of HashMap)
-//        Stream<Map.Entry<String, Integer>> topWords = wordsMap.entrySet().stream().
-//                sorted(Map.Entry.<String, Integer>comparingByValue().reversed());
-//
-//        //Now print the Top 5 words to console
-//        System.out.println("Top 5 Words:::");
-//        topWords.forEach(System.out::println);
-
-
+        Stream<Map.Entry<String, Integer>> topWordsFirst = uniqueWordMap
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed());
+        topWordsFirst.forEach(System.out::println);
     }
 }
